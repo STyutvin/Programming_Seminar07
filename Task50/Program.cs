cbd9a012ec71d4f4e1d105565d1513bcf3a9b479
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-27 16:34:51 +0300

    Оформление кода в стиле c#

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,49 +1,55 @@
-﻿//Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-void PrintArray(int[,] twodimArray) //метод вывода массива на печать
+﻿internal class Program
 {
-    for (int i = 0; i < twodimArray.GetLength(0); i++) //цикл для задания строк
+    private static void Main(string[] args)
     {
-        for (int j = 0; j < twodimArray.GetLength(1); j++) //цикл для задания столбцов
+        //Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+        void PrintArray(int[,] twodimArray) //метод вывода массива на печать
         {
-            Console.Write($"{twodimArray[i, j]} ");
+            for (int i = 0; i < twodimArray.GetLength(0); i++) //цикл для задания строк
+            {
+                for (int j = 0; j < twodimArray.GetLength(1); j++) //цикл для задания столбцов
+                {
+                    Console.Write($"{twodimArray[i, j]} ");
+                }
+                Console.WriteLine();
+            }
         }
-        Console.WriteLine();
-    }
-}
 
-void FillArray(int[,] twodimArray) //метод заполнения массива
-{
-    for (int i = 0; i < twodimArray.GetLength(0); i++) //цикл для задания строк
-    {
-        for (int j = 0; j <  twodimArray.GetLength(1); j++) //цикл для задания столбцов
+        void FillArray(int[,] twodimArray) //метод заполнения массива
         {
-            twodimArray[i, j] = new Random().Next(0,10);
+            for (int i = 0; i < twodimArray.GetLength(0); i++) //цикл для задания строк
+            {
+                for (int j = 0; j < twodimArray.GetLength(1); j++) //цикл для задания столбцов
+                {
+                    twodimArray[i, j] = new Random().Next(0, 10);
+                }
+            }
         }
-    }
-}
-Random rndRows = new Random();
-Random rndcolumns = new Random();
-int[,] matrix = new int[rndRows.Next(2,3), rndcolumns.Next(3,5)];
-FillArray(matrix);
-Console.Write("Задайте индекс элемента по строке: ");
-int rowIndex = Convert.ToInt32(Console.ReadLine());
-Console.Write("Задайте индекс элемента по столбцу: ");
-int columnIndex = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-Console.Write("Создаю случайный двумерный массив... ");
-Console.WriteLine("Массив создан!");
-PrintArray(matrix);
-Console.WriteLine();
-bool flag=false; //счетчик для выполнения условия по нахождению нужного элемента в массиве
-for (int i = 0; i < matrix.GetLength(0); i++) //цикл для задания строк
-{
-    for (int j = 0; j < matrix.GetLength(1); j++) //цикл для задания столбцов
-    {
-        if (i==rowIndex && j==columnIndex)
+        Random rndRows = new Random();
+        Random rndcolumns = new Random();
+        int[,] matrix = new int[rndRows.Next(2, 3), rndcolumns.Next(3, 5)];
+        FillArray(matrix);
+        Console.Write("Задайте индекс элемента по строке: ");
+        int rowIndex = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Задайте индекс элемента по столбцу: ");
+        int columnIndex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Создаю случайный двумерный массив... ");
+        Console.WriteLine("Массив создан!");
+        PrintArray(matrix);
+        Console.WriteLine();
+        bool flag = false; //счетчик для выполнения условия по нахождению нужного элемента в массиве
+        for (int i = 0; i < matrix.GetLength(0); i++) //цикл для задания строк
         {
-            flag=true;
-        }              
+            for (int j = 0; j < matrix.GetLength(1); j++) //цикл для задания столбцов
+            {
+                if (i == rowIndex && j == columnIndex)
+                {
+                    flag = true;
+                }
+            }
+        }
+        if (flag == true) Console.WriteLine("Значение элемента по указанному индексу: " + matrix[rowIndex, columnIndex]);
+        else Console.WriteLine("По указанному индексу элемента в массиве нет.");
     }
 }
-if (flag==true) Console.WriteLine("Значение элемента по указанному индексу: " + matrix[rowIndex,columnIndex]);
-else Console.WriteLine("По указанному индексу элемента в массиве нет.");
